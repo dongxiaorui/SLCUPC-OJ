bce0dcdaed6d2763c1930e86b0ed7e4b879ac1b7
dongxiaorui <31590874+dongxiaorui@users.noreply.github.com>
2019-01-05 12:38:24 +0800

    Add files via upload

--- 1562【整型数组】找最值.cpp
+++ 1562【整型数组】找最值.cpp
@@ -1,17 +1,16 @@
-main()
-{
-int a[10];
-int i,max,min;
-for(i=0;i<10;i++)
-scanf("%d",&a[i]);
-max=min=a[0];
-for(i=0;i<10;i++)
-{
- if(max<a[i]) max=a[i];
- if(min>a[i]) min=a[i];
-}
-printf("%d\n",max);
-printf("%d\n",min);
-for(i=0;i<10;i++)
-printf("%d ",a[i]);
+#include<stdio.h>
+int main(){
+	int a[10],max,min;
+	for(int i=0;i<10;i++)
+		scanf("%d",&a[i]);
+	max=a[0],min=a[0];
+	for(int i=0;i<10;i++){
+		max=max>a[i]?max:a[i];
+		min=min>a[i]?a[i]:min;
+	}
+	printf("%d\n",max);
+	printf("%d\n",min);
+	for(int i=0;i<10;i++)
+		printf("%d ",a[i]);
+	return 0;
 }
